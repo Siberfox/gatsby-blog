32463464e02198a208daa5d30e0feab0cf58c153
Andrew I <greyfoxjob@gmail.com>
2020-10-05 10:30:59 +0600

    minor styles fix

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ import React from "react"
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `rebeccapurple`,
+      background: "#f08a5d",
       marginBottom: `1.45rem`,
     }}
   >
